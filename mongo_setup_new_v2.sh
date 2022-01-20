94265e617a13fb6e43af7d35afa010b6d733de9b
Nguyen Nhat <nhat.nguyen@kingfood.co>
2022-01-21 02:13:06 +0700

    fix: fix config

--- mongo_setup_new_v2.sh
+++ mongo_setup_new_v2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "sleeping for 10 seconds"
-sleep 10
-
 echo mongo_setup.sh time now: `date +"%T" `
 mongo <<EOF
   var cfg = {
@@ -26,7 +24,10 @@ mongo <<EOF
     ],settings: {chainingAllowed: true}
   };
   rs.initiate(cfg, { force: true });
-  rs.status();
+  rs.reconfig(cfg, { force: true });
+  rs.secondaryOk();
+  db.getMongo().setReadPref('nearest');
+  db.getMongo().setSecondaryOk();
 EOF
 
 sleep 10
